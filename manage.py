7514b2c2fa38a38b91d9bfd8c97695faec38deca
Sajibul Islam Apon <76885203+SajibulIslamApon@users.noreply.github.com>
2025-09-01 15:32:50 +0000

    create default super user

--- manage.py
+++ manage.py
@@ -19,24 +19,7 @@ def main():
 
 
 
-from django.core.management.base import BaseCommand
-from django.contrib.auth import get_user_model
 
-
-class Command(BaseCommand):
-    help = 'Create default superuser from environment variables'
-
-    def handle(self, *args, **options):
-        User = get_user_model()
-        username = 'SuperApon'
-        email = 'superapon@gmail.com'
-        password = 'superapon'
-
-        if not User.objects.filter(username=username).exists():
-            User.objects.create_superuser(username=username, email=email, password=password)
-            self.stdout.write(self.style.SUCCESS(f'Superuser "{username}" created'))
-        else:
-            self.stdout.write(f'Superuser \"{username}\" already exists")
 
 
 
